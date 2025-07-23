bb26ed7e8f75a50063f629224b1aa69169a94a71
Thomas VINCENT <thomas.vincent@caramail.com>
2025-07-23 11:42:43 +0200

    Update to Minecraft 1.21

--- src/main/java/ovh/feelzor/fleeinganimals/mixin/LivingEntityMixin.java
+++ src/main/java/ovh/feelzor/fleeinganimals/mixin/LivingEntityMixin.java
@@ -1,34 +1,45 @@
 package ovh.feelzor.fleeinganimals.mixin;
 
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ovh.feelzor.fleeinganimals.FleeingAnimals;
+import ovh.feelzor.fleeinganimals.mixin.accessors.LivingEntityAccessor;
 
 import static ovh.feelzor.fleeinganimals.FleeingAnimals.getConfig;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
+
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setAttacker(Lnet/minecraft/entity/LivingEntity;)V"), method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z")
 	private void propagateAttacker(DamageSource source, float amount, CallbackInfoReturnable<Boolean> ci) {
 		// The method invokes setAttacker when attacker is known to be a LivingEntity.
 		LivingEntity attacker = (LivingEntity) source.getAttacker();
-
 		if (!((Object) this instanceof PassiveEntity thisPassiveMob)) return;
 
 		Class<? extends PassiveEntity> afraidClass = (!getConfig().sameSpeciesOnly) ? PassiveEntity.class : thisPassiveMob.getClass();
 
 		double d = thisPassiveMob.getAttributeValue(EntityAttributes.GENERIC_FOLLOW_RANGE) * getConfig().radius;
+
 		Box box = Box.from(thisPassiveMob.getPos()).expand(d, getConfig().yRadius, d);
 		thisPassiveMob.getWorld().getEntitiesByClass(afraidClass, box, EntityPredicates.EXCEPT_SPECTATOR).stream()
 				.filter(mob -> mob != thisPassiveMob) // Other mobs only
 				.filter(mob -> mob.getAttacker() == null) // Not already attacked
-				.forEach(mob -> mob.setAttacker(attacker));
+				.forEach(mob -> {
+					// From 1.21, the EscapeDangerGoal uses the last damage source and time to determine if the mob is fleeing.
+					((LivingEntityAccessor) mob).setLastDamageSource(source);
+					((LivingEntityAccessor) mob).setLastDamageTime(mob.getWorld().getTime());
+				});
 	}
 }
---
+++ src/main/java/ovh/feelzor/fleeinganimals/mixin/accessors/LivingEntityAccessor.java
@@ -0,0 +1,15 @@
+package ovh.feelzor.fleeinganimals.mixin.accessors;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityAccessor {
+    @Accessor("lastDamageSource")
+    public void setLastDamageSource(DamageSource source);
+
+    @Accessor("lastDamageTime")
+    public void setLastDamageTime(long time);
+}
